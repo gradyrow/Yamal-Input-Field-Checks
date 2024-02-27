6f11390c06e08884e99dacee2532c3ea4b794208
Grayden Rowedder <70239212+gradyrow@users.noreply.github.com>
2024-02-26 20:27:56 -0700

    Added more commands that will be used

--- Check_Inputs.py
+++ Check_Inputs.py
@@ -3,15 +3,18 @@ import os
 
 
 def bad_input_exception(input, pattern, invalid_message):
-    if pattern.match(input) is None:
-        print(invalid_message)  # bad __ input, create exception
-
+    try:
+        if pattern.match(input) is None:
+            print(invalid_message)  # bad __ input, create exception
+    except TypeError:
+        print(invalid_message)
 
 def hostInput(input):
-    host = re.compile('^[a-zA-Z0-9 ]+$|^\d{3}\.\d{3}\.\d{3}\.\d{3}$')
-    bad_input_exception(input, host, "Bad host")
+    if input is not None:
+        host = re.compile('^[a-zA-Z0-9 ]+$|^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$')
+        bad_input_exception(input, host, "Bad host")
 
-hostInput("129.132.147.128")
+hostInput("169.0.0.4")
 
 
 def stateInput(input):
@@ -19,15 +22,15 @@ def stateInput(input):
     bad_input_exception(input, state, "Bad state input")
 
 
-stateInput("present")
+    stateInput("present")
 
 
-def nameInput(input):
-    name = re.compile('^[a-zA-Z0-9 ,.?!@#$%^&*();:/_+=-]+$')
-    bad_input_exception(input, name, "Bad name input")
+def nameSubjectInput(input):
+    name = re.compile("^[a-zA-Z0-9 ,.?!@#$%^&*()<'>;:/_+=-]+$")
+    bad_input_exception(input, name, "Bad name/subject input")
 
 
-nameInput("Home Host Server")
+nameSubjectInput("Home Host Server")
 
 
 def portInput(input):
@@ -45,13 +48,42 @@ emailInput("gradyrow@hotmail.com")
 
 
 def bodyMsgInput(input):
-    bodyMsg = re.compile('^"[a-zA-Z0-9 ,.?!@#$%^&*();:/_+=-]+"$')
+    bodyMsg = re.compile('^"[a-zA-Z0-9 ,.?!@#$%^&*()<>;:/_+=-]+"$')
     bad_input_exception(input, bodyMsg, "Bad body/msg input")
 
 
 bodyMsgInput('"This is and email notification"')
 
+def commandInput(input):
+    command = re.compile("^[a-zA-Z0-9 ,.?!@#$%^&*()<'>;:/_+=-]+$")
+    bad_input_exception(input, command, "Bad command input")
 
+
+commandInput("echo 'Hello from Ansible' > /tmp/output.txt")
+
+
+def countInput(input):
+    count = re.compile("^[0-9]+$")
+    bad_input_exception(input, count, "Bad count input")
+
+
+countInput("1")
+
+
+def unitInput(input):
+    unit = re.compile(r'\b(seconds|minutes|hours)\b', re.IGNORECASE)
+    bad_input_exception(input, unit, "Bad unit input")
+
+
+unitInput("minutes")
+
+
+def timespecInput(input):  # needs work
+    timespec = re.compile(r'\b(seconds|minutes|hours)\b', re.IGNORECASE)
+    bad_input_exception(input, timespec, "Bad timespec input")
+
+
+unitInput("minutes")
 
 def repoInput(input):
     repo = re.compile(r'^https://github\.com/[^/]+/[^/]+\.git$')
